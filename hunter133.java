a242ce88e9ca2209682f3dc1d11e077020e85c37
kaaruR <karthikaram@gmail.com>
2017-06-28 11:39:57 +0530

    Create hunter133.java

---
+++ hunter133.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class hunter133
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String arr[]=new String[2];
+for(int i=0;i<2;i++)
+{
+arr[i]=sc.next();
+}
+for(int j=1;j>=0;j--)
+System.out.print(arr[j]);
+}
+}
